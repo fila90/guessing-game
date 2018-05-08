65e90ee70dd241cd703925e772ea497f67ad01d7
fila90 <ivan.filipovic.fila@gmail.com>
2018-05-08 04:00:20 +0200

    Fix indentaion

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,30 @@ fn main() {
     let rand_num = rand::thread_rng().gen_range(1, 101);
 
     loop {
-    	println!("Please enter the nubmer:");
-	    let mut guess = String::new();
+        println!("Please enter the nubmer:");
+        let mut guess = String::new();
 
-	    io::stdin().read_line(&mut guess)
-	    	.expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-	    // let guess: u32 = guess.trim().parse()
-	    // 	.expect("Please enter a number!");
-	    let guess: u32 = match guess.trim().parse() {
-	    	Ok(num) => num,
-	    	Err(_) => {
-	    		println!("You can't enter string {}", guess);
-	    		continue;
-	    	},
-	    };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You can't enter a string {}", guess);
+                continue;
+            },
+        };
 
-	    println!("You guessed: {}", guess);
+        println!("Your guess: {}", guess);
 
-	    match guess.cmp(&rand_num) {
-	    	Ordering::Less => println!("Too small"),
-	    	Ordering::Greater => println!("Too big"),
-	    	Ordering::Equal => {
-	    		println!("Bravooo!");
-	    		println!("Your number: {}, Random number: {}", guess, rand_num);
-	    		break;
-	    	}
-	    }
+        match guess.cmp(&rand_num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Bravooo!");
+                println!("Your number: {}, Random number: {}", guess, rand_num);
+                break;
+            }
+        }
     }
 }
